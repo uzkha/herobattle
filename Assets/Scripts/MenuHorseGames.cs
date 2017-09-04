b151a152cc5ce0a50cdbbf9e7470cffd5facd618
Diogo <diogolbx@gmail.com>
2017-09-04 13:45:37 -0300

    First commit

---
+++ Assets/Scripts/MenuHorseGames.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHorseGames : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject txDevelopment;
+
+	// Use this for initialization
+	void Start () {
+		txDevelopment.SetActive (false);
+	}
+
+	public void AtivarTexto(){
+		txDevelopment.SetActive (true);
+	}
+
+}
