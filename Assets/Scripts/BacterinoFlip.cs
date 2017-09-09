3ded241c8f97d70882c7e75bab837cc142c94b19
Diogo <diogolbx@gmail.com>
2017-09-09 20:59:54 -0300

    Temporizador no level 1. Iniciado configuracao de moedas e bacterias no level1

---
+++ Assets/Scripts/BacterinoFlip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BacterinoFlip : MonoBehaviour {
+
+    public Transform bacterino;
+
+    // Use this for initialization
+    void Start () {
+
+        bacterino = gameObject.GetComponent<Transform>();
+
+        //flip
+        Vector3 scala = bacterino.localScale;
+        scala.x *= -1;
+        bacterino.localScale = scala;
+
+
+    }
+
+
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,7 +9,12 @@ public class UIManager : MonoBehaviour {
     public UIManager instance;
 
     [SerializeField]
-    private Text pontosUI, user;
+    private Text pontosUI, user, txtTempo;
+
+    public bool iniciarTempo;
+    public float auxTempo = 5f;
+    private int auxTempoSound = 5;
+
 
     private void Awake()
     {
@@ -58,11 +63,49 @@ public class UIManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-	}
+        txtTempo = GameObject.Find("TXT-Tempo").GetComponent<Text>();
+        iniciarTempo = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        //carrega contagem de tempo para inicio do jogo
+        if (iniciarTempo == true && auxTempo > 0)
+        {   
+
+            auxTempo -= Time.deltaTime;            
+
+            if(auxTempo < 1)
+            {
+                iniciarTempo = false;
+                GameManager.instance.gameStart = true; // para classe GameManager inciar a fase
+                txtTempo.text = "Começar";
+
+                StartCoroutine(DestroiTemporizador());
+
+            }
+            else
+            {
+                
+                txtTempo.text = auxTempo.ToString("0");
+            }
+
+            if (auxTempoSound !=  (int) auxTempo)
+            {
+                AudioManager.instance.SonsFXToca(2);
+                auxTempoSound = (int)auxTempo;
+            }
+          
+
+        }
 		
 	}
+
+
+    private IEnumerator DestroiTemporizador()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(txtTempo.gameObject);
+    }
 }
