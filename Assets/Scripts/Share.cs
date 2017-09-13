b532dd126cd8d3e9d4cb0b23bd3686e5b2a41d47
Diogo <diogolbx@gmail.com>
2017-09-13 19:49:18 -0300

    IDE abert

---
+++ Assets/Scripts/Share.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Share : MonoBehaviour {
+	
+	private const string FACEBOOK_APP_ID = "123456789000";
+	private const string FACEBOOK_URL = "http://www.facebook.com/dialog/feed";
+
+	public void ShareToFacebook (string linkParameter, string nameParameter, string captionParameter, string descriptionParameter, string pictureParameter, string redirectParameter)
+	{
+		Application.OpenURL (FACEBOOK_URL + "?app_id=" + FACEBOOK_APP_ID +
+			"&link=" + WWW.EscapeURL(linkParameter) +
+			"&name=" + WWW.EscapeURL(nameParameter) +
+			"&caption=" + WWW.EscapeURL(captionParameter) + 
+			"&description=" + WWW.EscapeURL(descriptionParameter) + 
+			"&picture=" + WWW.EscapeURL(pictureParameter) + 
+			"&redirect_uri=" + WWW.EscapeURL(redirectParameter));
+	}
+
+}
